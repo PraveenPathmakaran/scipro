0ee394c405d39a396f7f1e097af3944e711f75e3
Praveen <praveenpathmakaran@gmail.com>
2023-11-23 12:18:01 +0530

    get it implemented

---
+++ lib/injection_container.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:scipro/application/auth/sign_in/sign_in_bloc.dart';
+import 'package:scipro/domain/auth/i_auth_repository.dart';
+import 'package:scipro/infrastructure/auth/auth_repository.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> init() async {}
+
+void authenticationInit() {
+  //bloc
+
+  getIt.registerFactory<SignInBloc>(() => SignInBloc(getIt()));
+  //repository
+
+  getIt.registerLazySingleton<IAuthRepository>(
+      () => AuthRepository(getIt(), getIt()));
+  //data sources
+  //core
+  //external
+
+  getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+  getIt.registerLazySingleton<GoogleSignIn>(() => GoogleSignIn());
+}
