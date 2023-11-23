5e68ff95dd57dfb2a4059b4e03519f1ffdcac746
Praveen <praveenpathmakaran@gmail.com>
2023-11-23 17:42:37 +0530

    get it rearranged

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:scipro/application/auth/auth/auth_bloc.dart';
 import 'package:scipro/application/auth/sign_in/sign_in_bloc.dart';
 import 'package:scipro/domain/auth/i_auth_repository.dart';
 import 'package:scipro/infrastructure/auth/auth_repository.dart';
@@ -9,23 +10,24 @@ import 'package:scipro/infrastructure/auth/auth_repository.dart';
 final getIt = GetIt.instance;
 
 Future<void> initGetIt() async {
-  authenticationInit();
+  externalInit();
+  blocInit();
+  repositoryInit();
 }
 
-void authenticationInit() {
-  //bloc
-
-  getIt.registerFactory<SignInBloc>(() => SignInBloc(getIt()));
-  //repository
-
-  getIt.registerLazySingleton<IAuthRepository>(
-      () => AuthRepository(getIt(), getIt()));
-  //data sources
-  //core
-  //external
-
+void externalInit() {
   getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
   getIt.registerLazySingleton<GoogleSignIn>(() => GoogleSignIn());
   getIt.registerLazySingleton<FirebaseFirestore>(
       () => FirebaseFirestore.instance);
+}
+
+void blocInit() {
+  getIt.registerFactory<AuthBloc>(() => AuthBloc(getIt()));
+  getIt.registerFactory<SignInBloc>(() => SignInBloc(getIt()));
+}
+
+void repositoryInit() {
+  getIt.registerLazySingleton<IAuthRepository>(
+      () => AuthRepository(getIt(), getIt()));
 }
