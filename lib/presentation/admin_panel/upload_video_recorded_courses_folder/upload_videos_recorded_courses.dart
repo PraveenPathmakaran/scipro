4fb903fae8817df605c8ce75ec6b0c07d86b5037
ajay <ajayp005@gmail.com>
2023-11-28 15:49:02 +0530

    3commit

---
+++ lib/presentation/admin_panel/upload_video_recorded_courses_folder/upload_videos_recorded_courses.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UploadVideosRecordedCourses extends StatelessWidget {
+  const UploadVideosRecordedCourses({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
